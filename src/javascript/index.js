7781bda839b265e8e19153fb9fe5506a845d6f4e
Naya Al Tahan <naya.al.tahan@lego.com>
2021-01-30 13:42:05 +0100

    main page + server

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,19 @@
-console.log('Hi')
+var i = 0;
+var txt = 'I\'m Naya Al Tahan, and I\'m almost graduating as a Software Engineer from VIA University College, in Horsens, Denmark. I\'m also a Student Backend Developer with the LEGO Group. ';
+var speed = 50;
+
+
+(function () {
+  typeWriter();
+})();
+
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementsByClassName("lead")[0].innerHTML ;
+    document.getElementById("mainText").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 // Webpack uses this to work with directories
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const internalIp = require('internal-ip');
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
 
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
 module.exports = {
 
     // Path to your entry point. From this file Webpack will begin his work
-    entry: './src/javascript/index.js',
+    entry: ['./src/javascript/index.js','webpack-plugin-serve/client'],
 
     // Path and filename of your result bundle.
     // Webpack will bundle all JavaScript into this file
@@ -18,6 +20,15 @@ module.exports = {
 
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                use: [
+                  {
+                    loader: 'html-loader',
+                    options: { minimize: true }
+                  }
+                ]
+              },
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
@@ -37,25 +48,8 @@ module.exports = {
                 // The first loader will be applied after others
                 use: [
                     {
-                        // After all CSS loaders we use plugin to do his work.
-                        // It gets all transformed CSS and extracts it into separate
-                        // single bundled file
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
                         // This loader resolves url() and @imports inside CSS
-                        loader: "css-loader",
-                    },
-                    {
-                        // Then we apply postCSS fixes like autoprefixer and minifying
-                        loader: "postcss-loader"
-                    },
-                    {
-                        // First we transform SASS to standard CSS
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
+                        loader: "css-loader"
                     }
                 ]
 
@@ -78,15 +72,18 @@ module.exports = {
               }
         ]
     },
+
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
+        ///...
+        new Serve({
+          host: internalIp.v4()
         })
-    ],
+      ],
 
     // Default mode for Webpack is production.
     // Depending on mode Webpack will apply different things
     // on final bundle. For now we don't need production's JavaScript
     // minifying and other thing so let's set mode to development
-    mode: 'development'
+    mode: 'development',
+    watch: true
 };
