25226b1809f116e74268d52b06416dba54f78b6c
Naya Al Tahan <naya.al.tahan@lego.com>
2021-02-14 18:47:00 +0100

    add projects kinda

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,10 +1,13 @@
+import * as projectsHandler from '../javascript/projectsHandler';
+
 var i = 0;
 var txt = 'I\'m Naya Al Tahan, and I\'m almost graduating as a Software Engineer from VIA University College, in Horsens, Denmark. I\'m also a Student Backend Developer with the LEGO Group. ';
-var speed = 50;
+var speed = 60;
 
 
 (function () {
   typeWriter();
+  projectsHandler.CreateArvr()
 })();
 
 
